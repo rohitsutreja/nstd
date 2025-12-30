bf7e9753a607408d04d948dbc39b94a0a202d72c
rohitsutreja <rohitsutreja@gmail.com>
2025-12-30 17:36:06 +0530

    unique_ptr : add converting constructor and assignment

--- include/nstd/unique_ptr.h
+++ include/nstd/unique_ptr.h
@@ -1,4 +1,5 @@
-#include <utility> 
+#include <utility>
+#include <type_traits>
 #include <cstddef>
 
 namespace nstd {
@@ -7,13 +8,27 @@ namespace nstd {
 	class unique_ptr {
 	public:
 		constexpr unique_ptr() noexcept = default;
-
 		constexpr unique_ptr(std::nullptr_t) noexcept {}
+		constexpr explicit unique_ptr(T* ptr) noexcept : _ptr{ ptr } {}
+
+		template<typename U>
+		constexpr unique_ptr(unique_ptr<U>&& other) noexcept
+			requires(std::is_convertible_v<U*, T*>)
+		{
+			_ptr = other.release();
+		}
+
+
+		template<typename U>
+		unique_ptr& operator=(unique_ptr<U>&& other) noexcept
+			requires(std::is_convertible_v<U*, T*>)
+		{
+			reset(other.release());
+			return *this;
+		}
 
 		unique_ptr(const unique_ptr&) = delete;
 		void operator=(const unique_ptr&) = delete;
-
-		constexpr explicit unique_ptr(T* ptr) noexcept : _ptr{ ptr } {}
 
 		constexpr unique_ptr(unique_ptr&& other) noexcept : _ptr{ other._ptr } {
 			other._ptr = nullptr;
@@ -34,6 +49,17 @@ namespace nstd {
 
 		constexpr T& operator*() const noexcept {
 			return *_ptr;
+		}
+
+		constexpr void reset(T* new_ptr = nullptr) {
+			delete _ptr;
+			_ptr = new_ptr;
+		}
+
+		T* release() {
+			T* temp = _ptr;
+			_ptr = nullptr;
+			return temp;
 		}
 
 		constexpr ~unique_ptr() {
