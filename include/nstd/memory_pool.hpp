1193669d41c6b6967eccfd481657f0f9bdebd3cc
rohitsutreja <rohitsutreja@gmail.com>
2026-01-06 17:23:49 +0530

    memory_pool : use cast instead new placement operator

--- include/nstd/memory_pool.hpp
+++ include/nstd/memory_pool.hpp
@@ -1,17 +1,13 @@
+#ifndef NSTD_MEMORY_POOL
+#define NSTD_MEMORY_POOL
 
-#include <new>       
+#include <new>
+#include <algorithm>
+#include <vector>
 
 namespace nstd {
-
 	template<typename T, size_t BlocksPerChunk = 100>
 	class memory_pool {
-	private:
-		union Block {
-			T val;
-			Block* next;
-		};
-
-
 	public:
 		memory_pool() {
 			_expand();
@@ -22,21 +18,24 @@ namespace nstd {
 			if (!_head) {
 				_expand();
 			}
-
 			void* result{ _head };
 			_head = *reinterpret_cast<void**>(_head);
-			return new (result) T(std::forward<Args>(args)...);
+			try {
+				return new (result) T(std::forward<Args>(args)...);
+			}
+			catch (...) {
+				*reinterpret_cast<void**>(result) = _head;
+				_head = result;
+				throw;
+			}
 		}
 
 		void deallocate(T* ptr) {
 			if (!ptr) {
 				return;
 			}
-
 			ptr->~T();
-
-			new (ptr) void* { _head };
-
+			*reinterpret_cast<void**>(ptr) = _head;
 			_head = ptr;
 		}
 
@@ -46,34 +45,30 @@ namespace nstd {
 			}
 		}
 
-	private:
+		memory_pool(const memory_pool&) = delete;
+		memory_pool& operator=(const memory_pool&) = delete;
 
+	private:
 		void _expand() {
 			auto* const raw_mem{ ::operator new(block_size * BlocksPerChunk) };
 			_chunks.push_back(raw_mem);
 
 			char* ptr = static_cast<char*>(raw_mem);
 
-			for (int i{}; i < BlocksPerChunk - 1; ++i) {
-				new (ptr + (i * block_size)) void* { ptr + (i + 1) * block_size };
-
-				// OR
-
-				// std::construct_at(
-				// 	reinterpret_cast<void**>(ptr + (i * block_size)),
-				// 	ptr + (i + 1) * block_size
-				// );
+			for (size_t i{}; i < BlocksPerChunk - 1; ++i) {
+				*reinterpret_cast<void**>(ptr + (i * block_size)) =
+					ptr + ((i + 1) * block_size);
 			}
 
-
-			new (ptr + (BlocksPerChunk - 1) * block_size) void* { _head };
-
+			*reinterpret_cast<void**>(ptr + ((BlocksPerChunk - 1) * block_size)) = _head;
 			_head = raw_mem;
 		}
 
-
 		void* _head{};
 		nstd::vector<void*> _chunks{};
-		static constexpr size_t block_size{ std::max(sizeof(T), sizeof(void*)) };
+
+		static constexpr size_t block_size{ (std::max(sizeof(T), sizeof(void*))) };
 	};
 }
+
+#endif
