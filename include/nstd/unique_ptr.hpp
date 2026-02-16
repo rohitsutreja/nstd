5f150ead8a5935c978a2044bc01b3d166297a96a
rohitsutreja <rohitsutreja@gmail.com>
2026-02-16 18:56:20 +0530

    unique_ptr : add custom deleter

--- include/nstd/unique_ptr.hpp
+++ include/nstd/unique_ptr.hpp
@@ -8,44 +8,56 @@
 namespace nstd {
 
 	template<typename T>
+	struct default_delete {
+
+		constexpr default_delete() noexcept = default;
+
+		template <typename U>
+		constexpr default_delete(const default_delete<U>&) noexcept
+			requires(std::is_convertible_v<U*, T*>) {}
+
+
+		constexpr void operator()(T* ptr) const noexcept {
+			delete ptr;
+		}
+	};
+
+	template<typename T, typename Deleter = default_delete<T>>
 	class unique_ptr {
 	public:
 		constexpr unique_ptr() noexcept = default;
 		constexpr unique_ptr(std::nullptr_t) noexcept {}
+
 		constexpr explicit unique_ptr(T* ptr) noexcept : _ptr{ ptr } {}
+
+		constexpr unique_ptr(T* ptr, const Deleter& d) noexcept : _ptr{ ptr }, _deleter{ d } {}
+		constexpr unique_ptr(T* ptr, Deleter&& d) noexcept : _ptr{ ptr }, _deleter{ std::move(d) } {}
 
 		template<typename U>
 		constexpr unique_ptr(unique_ptr<U>&& other) noexcept
-			requires(std::is_convertible_v<U*, T*>)
-		{
-			_ptr = other.release();
-		}
-
+			requires(std::is_convertible_v<U*, T*>) : _ptr{ other.release() }, _deleter{ std::move(other.get_deleter()) } {}
 
 		template<typename U>
 		unique_ptr& operator=(unique_ptr<U>&& other) noexcept
 			requires(std::is_convertible_v<U*, T*>)
 		{
 			reset(other.release());
+			_deleter = std::move(other.get_deleter());
 			return *this;
 		}
 
 		unique_ptr(const unique_ptr&) = delete;
-		void operator=(const unique_ptr&) = delete;
+		unique_ptr& operator=(const unique_ptr&) = delete;
 
-		constexpr unique_ptr(unique_ptr&& other) noexcept : _ptr{ other._ptr } {
-			other._ptr = nullptr;
-		}
+		constexpr unique_ptr(unique_ptr&& other) noexcept : _ptr{ std::exchange(other._ptr, nullptr) }, _deleter{ std::move(other._deleter) } {}
 
 		constexpr unique_ptr& operator=(unique_ptr&& other) noexcept {
 			if (this != &other) {
-				delete _ptr;
-				_ptr = other._ptr;
-				other._ptr = nullptr;
+				reset(other.release());
+				_deleter = std::move(other._deleter);
 			}
 			return *this;
 		}
-
 		constexpr T* operator->() const noexcept {
 			return _ptr;
 		}
@@ -58,23 +70,37 @@ namespace nstd {
 			return _ptr;
 		}
 
-		constexpr void reset(T* new_ptr = nullptr) {
-			delete _ptr;
-			_ptr = new_ptr;
+		constexpr T* get() const noexcept {
+			return _ptr;
 		}
 
-		T* release() {
+
+		constexpr Deleter& get_deleter() noexcept { return _deleter; }
+		constexpr const Deleter& get_deleter() const noexcept { return _deleter; }
+
+
+		constexpr void reset(T* new_ptr = nullptr) noexcept {
+			auto* const old_ptr{ std::exchange(_ptr, new_ptr) };
+			if (old_ptr != nullptr) {
+				_deleter(old_ptr);
+			}
+		}
+
+		constexpr T* release() noexcept {
 			T* temp = _ptr;
 			_ptr = nullptr;
 			return temp;
 		}
 
 		constexpr ~unique_ptr() {
-			delete _ptr;
+			if (_ptr != nullptr) {
+				_deleter(_ptr);
+			}
 		}
 
 	private:
 		T* _ptr{};
+		[[no_unique_address]] Deleter _deleter{};
 	};
 
 	template<typename T, typename ...Args>
